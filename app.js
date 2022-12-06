e5cdbe3b10312b2f1d6452b437a68e682ff39660
Arthur Dias <dias.arthurandrade50@gmail.com>
2022-12-06 15:10:51 -0300

    Voltando versão

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
 console.log("Conectando banco de dados ao gerenciamento")
 console.log("Configurando console")
-console.log("Configurando textos")
+
